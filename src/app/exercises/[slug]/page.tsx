ec53871efb0ec60038d08df7b13861b60b338b4d
Nour Adel <nour@Nours-Air.attlocal.net>
2024-03-12 13:45:49 -0500

    ADDED: pages structure

---
+++ src/app/exercises/[slug]/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ExercisePage: React.FC = () => {
+  return (
+    <div>
+      <h1>Next.js Page</h1>
+      <p>This is a basic Next.js page.</p>
+    </div>
+  );
+};
+
+export default ExercisePage;
